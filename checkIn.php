b114a5f88bcd5e0861bb5ae94e495488d5999927
DigitalMoleMan <digitalmoleguy@gmail.com>
2019-09-17 11:56:10 +0000

    Added bot response template to checkIn.php

--- checkIn.php
+++ checkIn.php
@@ -1,3 +1,34 @@
 <?php
-
+	
+	// Authorized team tokens that you would need to get when creating a slash command. Same script can serve multiple teams, just keep adding tokens to the array below.
+	$tokens = array(
+		"tW0wHpLokfme5zJppcZSJUDg"
+	);
+	
+	// check auth
+	if (!in_array($_REQUEST['token'], $tokens)) {
+		bot_respond("*Unauthorized token!*");
+		die();
+	}
+	
+    // Basic use of post data slack sends
+    $inputs = explode(" ", $_REQUEST['text']);
+    bot_respond($inputs[0]);
+	// This can used for debugging
+	dumper($_REQUEST);
+	
+	
+	/*
+		Helper Functions
+	*/
+	
+	// Send information back to slack
+	function bot_respond($output){
+		echo json_encode($output);
+	}
+	
+	function dumper($request){
+        echo "<pre>" . print_r($request, 1) . "</pre>";
+    }
+	
 ?>
