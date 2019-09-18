ee1d4f0f1ed1d48e10488fa5a952cd1ecff61ba1
Lacertilia <caramba97979@hotmail.com>
2019-09-18 10:37:27 +0200

    Added basic structure

--- teamAddMember.php
+++ teamAddMember.php
@@ -1,3 +1,19 @@
 <?php
 
+    $tokens = array(
+        "P2zoHA16O3ZuQQpQYpE7EC7M"
+    );
+
+    if(!in_array($_REQUEST['token'], $tokens)){
+        bot_respond("Unauthorized Token!");
+        die();
+    }
+
+    if(!isset($_POST['text'])){
+        bot_respond('Please write a team and user name.');
+        die();
+    }
+
+    
+
 ?>
