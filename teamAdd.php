46cde4941d199a3c408f4b54836481eea8e5a6c0
Lacertilia <caramba97979@hotmail.com>
2019-09-18 09:49:13 +0200

    Added code in teamAdd

--- teamAdd.php
+++ teamAdd.php
@@ -1,6 +1,6 @@
 <?php
     $tokens = array(
-        ""
+        "P2zoHA16O3ZuQQpQYpE7EC7M"
     );
 
     if(!in_array($_REQUEST['token'], $tokens)){
@@ -8,12 +8,22 @@
         die();
     }
 
-    $teamName = $_POST['text'];
+    if(!isset($_POST['text'])){
+        bot_respond('Please write a team name.');
+        die();
+    }
+
+    $filteredTeamName = filter_input(INPUT_POST, "text", FILTER_SANITIZE_STRING);
 
     include_once 'include/dbinfo.php';
 
 
-    $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname . ';charset=utf8mb4', $dbuser, $dbpass);
+    $stmt = $dbh->prepare("INSERT INTO `teams`(`id`, `name`) VALUES name = :name");
+    $stmt->bindParam(':name', $filteredTeamName);
+    $stmt->execute();
+
+    bot_respond('Team "' . $filteredTeamName . '" sucessfully created.');
+    die();
 
 
 
