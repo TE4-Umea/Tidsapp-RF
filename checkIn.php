e3bf6ba8fbca372c30e920e14158de820c6db8b7
digitalmoleman <digitalmoleguy@gmail.com>
2019-09-19 09:11:25 +0200

    moved fetching to functions

--- checkIn.php
+++ checkIn.php
@@ -1,55 +1,72 @@
 <?php
-     
-	// Authorized team tokens that you would need to get when creating a slash command. Same script can serve multiple teams, just keep adding tokens to the array below.
-	$tokens = array(
-		"tW0wHpLokfme5zJppcZSJUDg"
-	);
-	
-	// check auth
-	if (!in_array($_REQUEST['token'], $tokens)) {
-		bot_respond("*Unauthorized token!*");
-		die();
-	}
-    
+
+// Authorized team tokens that you would need to get when creating a slash command. Same script can serve multiple teams, just keep adding tokens to the array below.
+$tokens = array(
+	"tW0wHpLokfme5zJppcZSJUDg"
+);
+
+// check auth
+if (!in_array($_REQUEST['token'], $tokens)) {
+	bot_respond("*Unauthorized token!*");
+	die();
+}
+
 //split arguments into array.
-    $args = explode(" ", $_REQUEST['text']); 
- //Throw error if there are too many arguments.
-   if(count($args) > 1) die("Too many arguments.");
-   else {
-    if($args[0] == ""){
-        //TODO: Set user to active on "other".
-    } else {
-        
-        //TODO: Set any active project to inactive.
-        //TODO: Set user to active on project.
-       
-    
-    	//Check if there are no arguments.
+$args = explode(" ", $_REQUEST['text']);
+//Throw error if there are too many arguments.
+if (count($args) > 1) die("Too many arguments.");
+else {
+	if ($args[0] == "") {
+		//TODO: Set user to active on "other".
+	} else {
+
+		//TODO: Set any active project to inactive.
+		//TODO: Set user to active on project.
+		$proj_name = $args[0]; // first argument specifes project name.
+
+		//Check if there are no arguments.
 		include_once 'include/dbinfoExample.php';
-		$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-		
-		$sql = "SELECT id FROM projects WHERE name = :name";
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+		// get project id of the specified project.
 		bot_respond($args[0]);
-		$stmt = $dbh->prepare($sql);
-		$stmt->bindParam(':name', $args[0]);
-		$stmt->execute(); 
-		$result = $stmt->fetch(PDO::FETCH_ASSOC);
-		$sql = "SELECT * FROM projectMeta WHERE projectId = :projectId";
-		$stmt = $dbh->prepare($sql);
-		$stmt->bindParam(':projectId', $result[0]);
-		$stmt->execute(); 
-		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		$proj_id = get_project_id($dbh, $proj_name);
+
+		$proj_meta = get_project_meta($dbh, $proj_id);
 		bot_respond($result);
-		
-		}
 	}
-	
-	/*
+}
+
+//fetch the id of the specified project from database using projectName.
+function get_project_id($pdo, $project_name)
+{
+	$sql = "SELECT id FROM projects WHERE name = :name";
+	$stmt = $pdo->prepare($sql);
+	$stmt->bindParam(':name', $project_name);
+	$stmt->execute();
+	$result = $stmt->fetch(PDO::FETCH_ASSOC);
+	return $result;
+}
+
+//fetch the projectMeta of the specified project from database using projectId.
+function get_project_meta($pdo, $project_id)
+{
+	$sql = "SELECT * FROM projectMeta WHERE projectId = :projectId";
+	$stmt = $pdo->prepare($sql);
+	$stmt->bindParam(':projectId', $project_id);
+	$stmt->execute();
+	$result = $stmt->fetch(PDO::FETCH_ASSOC);
+	return $result;
+}
+
+/*
 		Helper Functions
 	*/
-	
-	// Send information back to slack
-	function bot_respond($output){
-		echo json_encode($output . "<br>");
-	}
-?>
+
+// Send information back to slack
+function bot_respond($output)
+{
+	echo json_encode($output . "<br>");
+}
