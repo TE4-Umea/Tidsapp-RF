47b7d03607eaab695cd23b006c74a02d2140bd01
digitalmoleman <digitalmoleguy@gmail.com>
2019-09-19 09:23:43 +0200

    commenting

--- checkIn.php
+++ checkIn.php
@@ -16,6 +16,8 @@ $args = explode(" ", $_REQUEST['text']);
 //Throw error if there are too many arguments.
 if (count($args) > 1) die("Too many arguments.");
 else {
+
+	// Check if there are no arguments.
 	if ($args[0] == "") {
 		//TODO: Set user to active on "other".
 	} else {
@@ -24,22 +26,22 @@ else {
 		//TODO: Set user to active on project.
 		$proj_name = $args[0]; // first argument specifes project name.
 
-		//Check if there are no arguments.
+
 		include_once 'include/dbinfoExample.php';
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-		// get project id of the specified project.
 		bot_respond($args[0]);
 
+		// get project id.
 		$proj_id = get_project_id($dbh, $proj_name);
 
+		// get project meta.
 		$proj_meta = get_project_meta($dbh, $proj_id);
 		bot_respond($result);
 	}
 }
 
-//fetch the id of the specified project from database using projectName.
+// fetch the id of the specified project from database using projectName.
 function get_project_id($pdo, $project_name)
 {
 	$sql = "SELECT id FROM projects WHERE name = :name";
@@ -50,7 +52,7 @@ function get_project_id($pdo, $project_name)
 	return $result;
 }
 
-//fetch the projectMeta of the specified project from database using projectId.
+// fetch the projectMeta of the specified project from database using projectId.
 function get_project_meta($pdo, $project_id)
 {
 	$sql = "SELECT * FROM projectMeta WHERE projectId = :projectId";
@@ -62,11 +64,11 @@ function get_project_meta($pdo, $project_id)
 }
 
 /*
-		Helper Functions
-	*/
+	Helper Functions
+*/
 
 // Send information back to slack
 function bot_respond($output)
 {
-	echo json_encode($output . "<br>");
+	echo json_encode($output) . "<br>";
 }
