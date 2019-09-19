30f72cdc641c90e57a2fca957448d88a3b1f424e
digitalmoleman <digitalmoleguy@gmail.com>
2019-09-19 10:32:13 +0200

    get user meta

--- checkIn.php
+++ checkIn.php
@@ -11,17 +11,25 @@ if (!in_array($_REQUEST['token'], $tokens)) {
 	die();
 }
 
+$user_user_id = $_REQUEST['user_id'];
+
 //split arguments into array.
 $args = explode(" ", $_REQUEST['text']);
 
 //Throw error if there are too many arguments.
 if (count($args) > 1) die("Too many arguments.");
 else {
-	$user_id = $_REQUEST['user_id'];
 	// Load database info from dbinfo.
 	include_once 'include/dbinfoExample.php';
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
+
+	//get user id.
+	$user_id = get_user_id($dbh, $user_user_id);
+	bot_respond($user_id);
+
+	$user_meta = get_user_meta($dbh, $user_id);
+	bot_respond($user_meta);
+
 	// Check if there are no arguments.
 	if ($args[0] == "") {
 		//TODO: Set user to active on "other".
@@ -41,6 +49,29 @@ else {
 		bot_respond($proj_meta);
 	}
 }
+// fetch the id of the user from database using user_id.
+function get_user_id($pdo, $user_user_id){
+	$sql = "SELECT id FROM users WHERE userId = :userId";
+	$stmt = $pdo->prepare($sql);
+	$stmt->bindParam(':userId', $user_user_id);
+	$stmt->execute();
+	$result =  array_values($stmt->fetch(PDO::FETCH_ASSOC))[0];
+	if ($result == false) die("Could not find user id");
+	else return $result;
+}
+
+// fetch the projectMeta of the specified project from database using projectId.
+function get_user_meta($pdo, $user_id)
+{
+	$sql = "SELECT * FROM userMeta WHERE userId = :userId";
+	$stmt = $pdo->prepare($sql);
+	$stmt->bindParam(':userId', $user_id);
+	$stmt->execute();
+	$result = $stmt->fetch(PDO::FETCH_ASSOC);
+	if ($result == false) die("Could not find user meta.");
+	else return $result;
+}
+
 
 // fetch the id of the specified project from database using projectName.
 function get_project_id($pdo, $project_name)
@@ -50,7 +81,7 @@ function get_project_id($pdo, $project_name)
 	$stmt->bindParam(':name', $project_name);
 	$stmt->execute();
 	$result =  array_values($stmt->fetch(PDO::FETCH_ASSOC))[0];
-	if($result == false) die("Could not find project id");
+	if ($result == false) die("Could not find project id");
 	else return $result;
 }
 
@@ -62,7 +93,7 @@ function get_project_meta($pdo, $project_id)
 	$stmt->bindParam(':projectId', $project_id);
 	$stmt->execute();
 	$result = $stmt->fetch(PDO::FETCH_ASSOC);
-	if($result == false) die("Could not find project meta.");
+	if ($result == false) die("Could not find project meta.");
 	else return $result;
 }
 
@@ -73,6 +104,6 @@ function get_project_meta($pdo, $project_id)
 // Send information back to slack
 function bot_respond($output)
 {
-	echo (array_values($output)) . "<br>";
+	echo ($output) . "<br>";
 	echo json_encode($output) . "<br><br>";
 }
