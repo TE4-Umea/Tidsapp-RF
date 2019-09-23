44f3e657dd1f86752b8417f35a2e0dbfc39f1d97
Lacertilia <caramba97979@hotmail.com>
2019-09-23 15:26:02 +0200

    Added base code

--- checkOut.php
+++ checkOut.php
@@ -1,3 +1,35 @@
 <?php
 
+    include_once 'include/dbinfo.php';
+
+    $u_id = filter_var($_REQUEST['user_id'], FILTER_SANITIZE_STRING);
+   
+    $true = 1;
+    $active = 0;
+
+    $stmt = $dbh->prepare("SELECT checkedInAt FROM projectConnections WHERE userId = :userId AND active = :true");
+    $stmt->bindParam(':userId', $u_id);
+    $stmt->bindParam(':true', $true);
+    $stmt->execute();
+
+    $chekedInAt = array_values($stmt->fetch(PDO::FETCH_ASSOC))[0];
+
+    $stmt = $dbh->prepare("SELECT timeSpent FROM projectConnections WHERE userId = :userId AND active = :true");
+    $stmt->bindParam(':userId', $u_id);
+    $stmt->bindParam(':true', $true);
+    $stmt->execute();
+
+    $timeSpent = array_values($stmt->fetch(PDO::FETCH_ASSOC))[0];
+
+    $timeSpent += (time() - $chekedInAt);
+
+    botRespond("timeSpent", $timeSpent);
+
+    $stmt = $dbh->prepare("UPDATE projectConnections SET active = :active, timeSpent = :timeSpent WHERE userId = :userId AND active = :true");
+    $stmt->bindParam(':userId', $u_id);
+    $stmt->bindParam(':true', $true);
+    $stmt->bindParam(':active', $active);
+    $stmt->bindParam(':timeSpent', $timeSpent);
+    $stmt->execute();
+
 ?>
