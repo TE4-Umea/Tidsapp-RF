fa3ade96a1b4701c74e67de3caac0a6883086353
Lacertilia <caramba97979@hotmail.com>
2019-09-23 11:14:18 +0200

    Added checks for if project is not in database

--- checkActive.php
+++ checkActive.php
@@ -10,11 +10,21 @@
 
     $projectId = $stmt->fetch(PDO::FETCH_ASSOC)[0];
 
+    if($projectId == false){
+       bot_respond('No currently active projects.');
+       die(); 
+    }
+
     $stmt = $dbh->prepare('SELECT `name` FROM projects WHERE id = :id');
     $stmt->bindParam(':id', $projectId);
     $stmt->execute();
 
     $projectName = $stmt->fetch(PDO::FETCH_ASSOC)[0];
+
+    if($projectName == false){
+        bot_respond('No currently active projects');
+        die();
+    }
 
     bot_respond('Currently active project is: ' . $projectName);
 
