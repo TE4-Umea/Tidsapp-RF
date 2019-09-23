c9b45cd396935339485a0ea010a7c6e4b895db60
DigitalMoleMan <digitalmoleguy@gmail.com>
2019-09-23 08:44:00 +0000

    dbinfo gone

--- include/dbinfoExample.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbuser = 'rfBot';
-$dbpass = 'rfBotPass';
-$dbhost = 'localhost';
-$dbname = 'testDB';
-
-$dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname . ';charset=utf8mb4', $dbuser, $dbpass);
-?>
