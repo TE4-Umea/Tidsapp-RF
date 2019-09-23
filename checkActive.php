6bf2251418ea0bc604eccc58d41f3884055c6f8f
Lacertilia <caramba97979@hotmail.com>
2019-09-23 11:07:52 +0200

    Added base code

--- checkActive.php
+++ checkActive.php
@@ -1,3 +1,25 @@
 <?php
+    include_once 'include/dbinfo.php';
 
+    $userId = $_POST['user_id'];
+
+    $stmt = $dbh->prepare('SELECT projectId FROM projectConnections(id, userId, projectId, active, checkedInAt, timeSpent) WHERE (userId = :userId, active = :active)');
+    $stmt->bindParam(':userId', $userId);
+    $stmt->bindParam(':active', 1);
+    $stmt->execute();
+
+    $projectId = $stmt->fetch(PDO::FETCH_ASSOC)[0];
+
+    $stmt = $dbh->prepare('SELECT `name` FROM projects WHERE id = :id');
+    $stmt->bindParam(':id', $projectId);
+    $stmt->execute();
+
+    $projectName = $stmt->fetch(PDO::FETCH_ASSOC)[0];
+
+    bot_respond('Currently active project is: ' . $projectName);
+
+    function bot_respond($message){
+        echo json_encode($message);
+    }
+    
 ?>
