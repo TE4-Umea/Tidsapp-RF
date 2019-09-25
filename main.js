1f11b8cfed4ec0dc5dd505d11928b1bfad496897
DigitalMoleMan <digitalmoleguy@gmail.com>
2019-09-25 10:53:37 +0000

    sign in with slack progress

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 
+
+
+var oAuthToken
+
+var slackClientId = "715933475586.763249056999";
+var slackClientSecret = "6765838853c54a3dab6cd33d4d4eac82";
+var slackCode = new String;
+var accessToken = new String;
 var userId = "FRONT";
 
+window.onload = () => {
+    slackCode = getSlackCode();
+    accessToken = getAccessToken();
+
+    console.log(accessToken);
+}
+
+
+getSlackCode = () => {
+    let params = new URLSearchParams(location.search);
+    return params.get('code');
+}
+
+getAccessToken = () => {
+    var token;
+    var request = `https://slack.com/api/oauth.access?client_id=${slackClientId}&client_secret=${slackClientSecret}&code=${slackCode}`
+    $.get(request, (appJson) => token = appJson);
+    return token;
+}
 
 getProjectNameInput = () => document.getElementById('projectNameInput').value;
 
