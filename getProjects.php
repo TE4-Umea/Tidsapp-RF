e4ea3e3014a075d48b036b88f0c5ae01b9699816
Lacertilia <caramba97979@hotmail.com>
2019-09-26 11:28:41 +0200

    Wrote getProjects.php

--- getProjects.php
+++ getProjects.php
@@ -1,8 +1,18 @@
 <?php
 
-include_once 'include/auth.php';
-include_once 'include/dbinfo.php';
+    include_once 'include/auth.php';
+    include_once 'include/dbinfo.php';
+
+    $stmt = $dbh->prepare('SELECT names FROM projects');
+    $stmt->execute();
+
+    $projects = $stmt->fetch();
+
+    bot_respond($projects);
 
 
+    function bot_respond($message){
+        echo json_encode($message);
+    }
 
 ?>
